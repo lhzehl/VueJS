4da70dd7c19c16dbd72dfce2bb013e5cbf7af2dd
batmanInTheJungle <german8895@gmail.com>
2020-05-28 09:21:35 +0300

    set store

--- moviesapp/src/store/modules/movies.js
+++ moviesapp/src/store/modules/movies.js
@@ -1,22 +1,56 @@
+import IDs from "@/store/mock/imdb_250";
 import axios from "@/plugins/axios";
+import mutations from "@/store/mutations";
+
+function serializeRespons(movies) {
+  return movies.reduce((acc, item) => {
+    acc[item.imdbID] = item;
+
+    return acc;
+  }, {});
+}
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+    movies: {},
+  },
+  getters: {
+    slicedIDs: ({ top250IDs }) => (from, to) => top250IDs.slice(from, to),
+
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
+  mutations: {
+    [MOVIES](state, value) {
+      state.movies = value;
+    },
+  },
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      //tt0111161
-      const response = await axios.get("/", {
-        params: {
-          apikey: process.env.VUE_APP_API_KEY,
-          plot: "full",
-          i: "tt0111161",
-        },
-      });
-      console.log(response);
+    async fetchMovies({ getters, commit }) {
+      //context => getters
+      try {
+        const { currentPage, moviesPerPage, slicedIDs } = getters;
+
+        const from = currentPage * moviesPerPage - moviesPerPage;
+
+        const to = currentPage * moviesPerPage;
+        const moviesToFetch = slicedIDs(from, to);
+
+        const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+
+        const response = await Promise.all(requests);
+        const movies = serializeRespons(response);
+
+        commit(MOVIES, movies);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
---
+++ moviesapp/src/store/mutations.js
@@ -0,0 +1,4 @@
+export default {
+  MOVIES: "MOVIES",
+  
+};
